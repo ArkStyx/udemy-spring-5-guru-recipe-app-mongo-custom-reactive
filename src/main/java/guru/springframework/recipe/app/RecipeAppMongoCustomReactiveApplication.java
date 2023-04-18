3360035c2fa2905e28b30b26d11561b10c32e298
ArkStyx <regis.petit@gmail.com>
2023-04-18 23:10:34 +0200

    Renommage de la classe App de Spring Boot

--- src/main/java/guru/springframework/recipe/app/RecipeAppMongoCustomReactiveApplication.java
+++ src/main/java/guru/springframework/recipe/app/RecipeAppMongoCustomReactiveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeAppApplication {
+public class RecipeAppMongoCustomReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeAppApplication.class, args);
+		SpringApplication.run(RecipeAppMongoCustomReactiveApplication.class, args);
 	}
 
 }
