b40ddf447c7478eff6a68ab932d7a80f27e3722c
ArkStyx <regis.petit@gmail.com>
2023-05-03 23:53:48 +0200

    Correctif sur le dernier test manquant

--- src/test/java/guru/springframework/recipe/app/repositories/UnitOfMeasureRepositoryIntegrationTest.java
+++ src/test/java/guru/springframework/recipe/app/repositories/UnitOfMeasureRepositoryIntegrationTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import guru.springframework.recipe.app.domain.UnitOfMeasure;
 
 @Disabled("Classe de tests seulement utilisable avec JPA")
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+@DataMongoTest
 class UnitOfMeasureRepositoryIntegrationTest {
 
 	@Autowired
